1daedc2bc93f7a8ff2209cddbbce5a2672124c5f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 18:01:40 -0400

    Adding many of the starting configuration variables for tada.

---
+++ tada/util/configuration.py
@@ -0,0 +1,19 @@
+"""Constants used by the tada package"""
+
+import os
+
+# Separators
+SEPARATOR = os.sep
+
+# Directories
+RESULTS = "_results"
+
+# Files
+CONFIGURATION = ".configuration.txt"
+
+# Configurations
+UTF8 = "utf-8"
+
+# Extensions
+JSON_EXT = ".json"
+PYTHON_EXT = ".py"
