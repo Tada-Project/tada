e31a22a39d9d64ffea39b6928d059dad6fb2e319
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 18:04:09 -0400

    Making a lot of revisions to the tada_a_bigoh so that it uses constants.

--- tada_a_bigoh.py
+++ tada_a_bigoh.py
@@ -1,19 +1,17 @@
 """Run doubling experiments and 'Tada!' you get the time complexity"""
 
+import perf
 import subprocess
 
-import perf
+from tada.util import configuration
 
+# Make available the package with functions-under-analysis
 import sys
-sys.path.insert(0, '/home/gkapfham/working/research/source/speed-surprises')
 
-UTF8 = "utf-8"
-CONFIGURATION = ".configuration.txt"
+sys.path.insert(0, "/home/gkapfham/working/research/source/speed-surprises")
+
+# TODO: the name of the experiment should be an argument
 PERF_EXPERIMENT_NAME = "perf_mcopies_ofc"
-JSON_EXT = ".json"
-PYTHON_EXT = ".py"
-RESULTS = "results"
-SEPARATOR = "/"
 
 
 def run_command(command):
@@ -46,26 +44,30 @@ if __name__ == "__main__":
     size = 100
     factor = 2
     size_stop = 200
-    save_configuration(CONFIGURATION, size)
+    save_configuration(configuration.CONFIGURATION, size)
     # perform the small doubling experiment
     while size <= size_stop:
         # run the benchmark by using it through python
         print("Start running experiment for size " + str(size) + " →\n")
         current_output, current_error = run_command(
-            "python3 " + PERF_EXPERIMENT_NAME + PYTHON_EXT
+            "python3 " + PERF_EXPERIMENT_NAME + configuration.PYTHON_EXT
         )
         # display the standard output and error
-        display_output(current_output.decode(UTF8))
-        display_output(current_error.decode(UTF8))
+        display_output(current_output.decode(configuration.UTF8))
+        display_output(current_error.decode(configuration.UTF8))
         # read the JSON file containing the results
         current_benchmark = perf.Benchmark.load(
-            RESULTS + SEPARATOR + PERF_EXPERIMENT_NAME + str(size) + JSON_EXT
+            configuration.RESULTS
+            + configuration.SEPARATOR
+            + PERF_EXPERIMENT_NAME
+            + str(size)
+            + configuration.JSON_EXT
         )
         # print('Values {0}'.format(current_benchmark.get_values()))
-        print('Mean {0}'.format(current_benchmark.mean()))
-        print('Median {0}'.format(current_benchmark.median()))
+        print("Mean {0}".format(current_benchmark.mean()))
+        print("Median {0}".format(current_benchmark.median()))
         print("→ Done running experiment for size " + str(size) + "\n")
         # go to the next size for the doubling experiment
         size = size * 2
         # write the next doubling experiment size to the file
-        save_configuration(CONFIGURATION, size)
+        save_configuration(configuration.CONFIGURATION, size)
