f699f07b45b5b7f4576986aab5f33da5136725e1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 20:25:36 -0400

    Adding the display_start_message to the display file.

--- tada/util/display.py
+++ tada/util/display.py
@@ -1,6 +1,11 @@
 """Display output for Tada"""
 
 
+def display_start_message(size):
+    """Display the start message for an experiment"""
+    print("Start running experiment for size " + str(size) + " →\n")
+
+
 def display_output(timing_output):
     """Display the timing output as long as it is not empty"""
     if timing_output != "":
