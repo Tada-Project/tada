0c26ed7937ab64cca25d54ae6639b83f728d9abf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 21:05:54 -0400

    Adding the first version of the arguments module and its two functions.

---
+++ tada/util/arguments.py
@@ -0,0 +1,21 @@
+"""Handle the arguments provided to Tada"""
+
+import argparse
+
+
+def parse(args):
+    """Use argparse to parse provided command-line arguments"""
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--directory", nargs="+", type=str)
+    arguments_finished = parser.parse_args(args)
+    return arguments_finished
+
+
+def verify(args):
+    """Verify the command-line arguments"""
+    verified_arguments = False
+    if args.directory is not None:
+        verified_arguments = True
+    return verified_arguments
