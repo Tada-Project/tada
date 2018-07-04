e1eb488d04e7f97ec22feb2accaccbe71ac6d273
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 20:21:50 -0400

    Adding the display file with the display_output function.

---
+++ tada/util/display.py
@@ -0,0 +1,7 @@
+"""Display output for Tada"""
+
+
+def display_output(timing_output):
+    """Display the timing output as long as it is not empty"""
+    if timing_output != "":
+        print(timing_output)
