422bd52b443d8176ba49d96535423d6bc4b20833
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-03 21:05:14 -0400

    Adding to the tada_a_bigoh all code for parsing arguments and verification.

--- tada_a_bigoh.py
+++ tada_a_bigoh.py
@@ -3,6 +3,7 @@
 import sys
 import perf
 
+from tada.util import arguments
 from tada.util import configuration
 from tada.util import display
 from tada.util import run
@@ -15,39 +16,48 @@ sys.path.insert(0, "/home/gkapfham/working/research/source/speed-surprises")
 PERF_EXPERIMENT_NAME = "perf_mcopies_ofc"
 
 if __name__ == "__main__":
-    # setup parameters of a simple doubling experiment
+    # TODO: move these to command-line arguments
     size = 100
     factor = 2
     size_stop = 100
     # display the welcome message
     display.welcome_message()
-    save.save_configuration(configuration.CONFIGURATION, size)
-    # perform the small doubling experiment
-    while size <= size_stop:
-        # run the benchmark by using it through python
-        display.start_message(size)
-        current_output, current_error = run.run_command(
-            configuration.PYTHON_EXEC
-            + configuration.SPACE
-            + PERF_EXPERIMENT_NAME
-            + configuration.PYTHON_EXT
-        )
-        # display the standard output and error
-        display.display_output(current_output.decode(configuration.UTF8))
-        display.display_output(current_error.decode(configuration.UTF8))
-        # read the JSON file containing the results
-        current_benchmark = perf.Benchmark.load(
-            configuration.RESULTS
-            + configuration.SEPARATOR
-            + PERF_EXPERIMENT_NAME
-            + str(size)
-            + configuration.JSON_EXT
-        )
-        # print('Values {0}'.format(current_benchmark.get_values()))
-        print("Mean {0}".format(current_benchmark.mean()))
-        print("Median {0}".format(current_benchmark.median()))
-        display.display_end_message(size)
-        # go to the next size for the doubling experiment
-        size = size * 2
-        # write the next doubling experiment size to the file
+    # read and verify the command-line arguments
+    tada_arguments = arguments.parse(sys.argv[1:])
+    did_verify_arguments = arguments.verify(tada_arguments)
+    # incorrect arguments, exit program
+    if did_verify_arguments is False:
+        print("Incorrect command-line arguments.")
+        sys.exit(configuration.INCORRECT_ARGUMENTS)
+    # correct arguments, run doubling experiment
+    else:
         save.save_configuration(configuration.CONFIGURATION, size)
+        # perform the small doubling experiment
+        while size <= size_stop:
+            # run the benchmark by using it through python
+            display.start_message(size)
+            current_output, current_error = run.run_command(
+                configuration.PYTHON_EXEC
+                + configuration.SPACE
+                + PERF_EXPERIMENT_NAME
+                + configuration.PYTHON_EXT
+            )
+            # display the standard output and error
+            display.display_output(current_output.decode(configuration.UTF8))
+            display.display_output(current_error.decode(configuration.UTF8))
+            # read the JSON file containing the results
+            current_benchmark = perf.Benchmark.load(
+                configuration.RESULTS
+                + configuration.SEPARATOR
+                + PERF_EXPERIMENT_NAME
+                + str(size)
+                + configuration.JSON_EXT
+            )
+            # print('Values {0}'.format(current_benchmark.get_values()))
+            print("Mean {0}".format(current_benchmark.mean()))
+            print("Median {0}".format(current_benchmark.median()))
+            display.display_end_message(size)
+            # go to the next size for the doubling experiment
+            size = size * 2
+            # write the next doubling experiment size to the file
+            save.save_configuration(configuration.CONFIGURATION, size)
