03998ecc816b96ba41ce2c17b44643af07f4b65a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-04 15:11:45 -0400

    Adding new test cases and a test name fix in test_arguments.

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -48,6 +48,14 @@ def test_directory_argument_verifiable(chosen_arguments):
     assert verified_arguments is True
 
 
+@pytest.mark.parametrize("chosen_arguments", [(["--directory", ""])])
+def test_directory_argument_not_verifiable(chosen_arguments):
+    """Check that valid directory arguments will verify correctly"""
+    parsed_arguments = arguments.parse(chosen_arguments)
+    verified_arguments = arguments.verify(parsed_arguments)
+    assert verified_arguments is False
+
+
 @pytest.mark.parametrize(
     "chosen_arguments",
     [
@@ -58,7 +66,7 @@ def test_directory_argument_verifiable(chosen_arguments):
         (["-directory", "d"]),
     ],
 )
-def test_directory_argument_not_verifiable(chosen_arguments, capsys):
+def test_directory_argument_not_verifiable_syserror(chosen_arguments, capsys):
     """Check that valid directory arguments will verify correctly"""
     with pytest.raises(SystemExit):
         arguments.parse(chosen_arguments)
