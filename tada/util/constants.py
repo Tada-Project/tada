6da1c8848c538b042526aba6dc6e4b0c8d0253e2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-04 12:50:41 -0400

    Adding the constants module that was previously configuration.

---
+++ tada/util/constants.py
@@ -0,0 +1,30 @@
+"""Constants used by the tada package"""
+
+import os
+
+# Separators
+SEPARATOR = os.sep
+SPACE = " "
+
+# Error codes
+INCORRECT_ARGUMENTS = 2
+
+# Directories
+RESULTS = "_results"
+
+# Files
+SIZE = ".size.txt"
+DIRECTORY = ".directory.txt"
+
+# constants.
+UTF8 = "utf-8"
+
+# Extensions
+JSON_EXT = ".json"
+PYTHON_EXT = ".py"
+
+# Commands
+PYTHON_EXEC = "python3"
+
+# Perf
+DESCRIPTION_METANAME = "description"
