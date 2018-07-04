fff7b7f7d7de3480bea256d2f2d6e0e9247a024d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-04 13:05:25 -0400

    Removing not-needed comments and adding TODO markers to perf_benchmark.

--- perf_benchmark.py
+++ perf_benchmark.py
@@ -10,28 +10,23 @@ from tada.util import read
 from tada.util import save
 
 if __name__ == "__main__":
+    # TODO: Generalize these to work for values read from configuration
     chosen_directory = read.read_directory()
     package.add_sys_path(chosen_directory)
     module = importlib.import_module("speedsurprises.text.copies")
     method = getattr(module, "mcopies_ofc")
-    # from speedsurprises.text import copies  # noqa: E402
-    # from m1 import copies  # noqa: E402
 
     # read the chosen_size
     chosen_size = read.read_experiment_size()
     # configure perf
     runner = perf.Runner()
     # configure the run of the benchmark
-    # TODO: make this based on the name of the experiment
+    # TODO: Make this based on the name of the experiment
     current_experiment_name = constants.PERF_BENCHMARK + str(chosen_size)
     runner.metadata[constants.DESCRIPTION_METANAME] = current_experiment_name
-    # run the perf benchmark for the function
-    # current_benchmark = runner.bench_func(
-    #     "mcopies", run.run_benchmark, copies.mcopies_ofc, chosen_size
-    # )
+    # TODO: Make this run the function read from the configuration
     current_benchmark = runner.bench_func(
         "mcopies", run.run_benchmark, method, chosen_size
     )
-
     # save the perf results from running the benchmark
     save.save_bencmark_results(current_benchmark, current_experiment_name)
