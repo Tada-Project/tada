9e63faae0819d100c8bd62bd6864d7520d1009a7
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-05 13:50:00 -0400

    Adding comments to the arguments function; including new argument soon.

--- tada/util/arguments.py
+++ tada/util/arguments.py
@@ -5,15 +5,24 @@ import argparse
 
 def parse(args):
     """Use argparse to parse provided command-line arguments"""
+    # create the parser with the default help formatter
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
+    # add all of the arguments to the command-line interface
     parser.add_argument(
         "--directory",
         required=True,
         type=str,
         help="Package directory with functions to analyze",
     )
+    # parser.add_argument(
+    #     "--module",
+    #     required=True,
+    #     type=str,
+    #     help="Module name with functions to analyze",
+    # )
+    # parse the arguments and return the finished result
     arguments_finished = parser.parse_args(args)
     return arguments_finished
 
