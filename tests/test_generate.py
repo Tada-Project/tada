436a34cc79b16dd1c71ce970d85cbd65af20123d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 13:00:43 -0400

    Adding a test to the test_generate suite for requesting two values.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -16,3 +16,17 @@ def test_generate_int_makes_size_default():
     # generate the data for the requested_types and the current_size
     generated_data = generate.generate_data(requested_types, current_size)
     assert generated_data == expected_tuple
+
+
+# pylint: disable=invalid-name
+def test_generate_ints_makes_size_default():
+    """Checks that requesting generated ints returns them"""
+    # request a single tuple with an int in it
+    requested_types = ["int", "int"]
+    # assume the doubling experiment is at 100
+    current_size = 100
+    # the default generator will return a tuple with 100 in it
+    expected_tuple = (current_size, current_size,)
+    # generate the data for the requested_types and the current_size
+    generated_data = generate.generate_data(requested_types, current_size)
+    assert generated_data == expected_tuple
