410e1cc5842b799790c9b16f46399b7f2a79399a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 14:43:13 -0400

    Adding the test_generate_char_makes_letter_default to test_generate.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -30,3 +30,17 @@ def test_generate_ints_makes_size_default():
     # generate the data for the requested_types and the current_size
     generated_data = generate.generate_data(requested_types, current_size)
     assert generated_data == expected_tuple
+
+
+# pylint: disable=invalid-name
+def test_generate_char_makes_letter_default():
+    """Checks that requesting a generated char returns one"""
+    # request a single tuple with an int in it
+    requested_types = ["char"]
+    # assume the doubling experiment is at 100; not needed for this test
+    current_size = 100
+    # the default generator will return a tuple with the default character in it
+    expected_tuple = (generate.DEFAULT_VALUE_CHAR,)
+    # generate the data for the requested_types and the current_size
+    generated_data = generate.generate_data(requested_types, current_size)
+    assert generated_data == expected_tuple
