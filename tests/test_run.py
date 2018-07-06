ac1599b6e2f7f858e745d8d48c66a5e9bee2486c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 12:17:20 -0400

    Removing an extra space in a docstring for the test case in the test_run.

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,7 +5,7 @@ from tada.util import run
 
 
 def test_run_command_returns_echo():
-    """Checks that a single line is returned from command """
+    """Checks that a single line is returned from command"""
     output, error = run.run_command('echo "Hello!"')
     assert error == b""
     assert output == b"Hello!\n"
