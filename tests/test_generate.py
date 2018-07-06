89a4daa222b9d65790e5712f13f42c2f4239a43d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-07-06 12:58:50 -0400

    Adding a full-featured and passing test case to the test_generate suite.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -7,6 +7,12 @@ from tada.util import generate
 # pylint: disable=invalid-name
 def test_generate_int_makes_size_default():
     """Checks that requesting a generated int returns one"""
+    # request a single tuple with an int in it
     requested_types = ["int"]
+    # assume the doubling experiment is at 100
     current_size = 100
-    generate.generate_data(requested_types, current_size)
+    # the default generator will return a tuple with 100 in it
+    expected_tuple = (current_size,)
+    # generate the data for the requested_types and the current_size
+    generated_data = generate.generate_data(requested_types, current_size)
+    assert generated_data == expected_tuple
