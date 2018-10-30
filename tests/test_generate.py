ffedbc5c8586aa4a6861c42f83d872bb0d71e74d
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-10-29 21:36:30 -0400

    Added  test_generate_boolean_makes_boolean_default

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -43,3 +43,17 @@ def test_generate_char_makes_letter_default():
     # generate the data for the requested_types and the current_size
     generated_data = generate.generate_data(requested_types, current_size)
     assert generated_data == expected_tuple
+
+
+# pylint: disable=invalid-name
+def test_generate_boolean_makes_boolean_default():
+    """Checks that requesting a generated char returns one"""
+    # request a single tuple with an int in it
+    requested_types = ["boolean"]
+    # assume the doubling experiment is at 100; not needed for this test
+    current_size = 100
+    # the default generator will return a tuple with the default character in it
+    expected_tuple = (generate.DEFAULT_VALUE_BOOLEAN,)
+    # generate the data for the requested_types and the current_size
+    generated_data = generate.generate_data(requested_types, current_size)
+    assert generated_data == expected_tuple
