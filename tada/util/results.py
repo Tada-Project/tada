a89de59c2f367dcb12a665e2aadfeee4261e4cdd
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2018-12-08 17:03:26 -0500

    Update results.py

--- tada/util/results.py
+++ tada/util/results.py
@@ -1,9 +1,9 @@
 """Resultstable for Tada and perf"""
 
-
-
 def add_resultstable(resultstable, current_size, mean, median):
+    """Add stuff into the resultstable"""
     resultstable.add_row([current_size, mean, median])
 
 def display_resultstable(resultstable):
+    """Print out resultstable"""
     print(resultstable)
