c27c73509dbafabc6d726a8072bddaeee7c21d4d
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-12-08 16:57:36 -0500

    Update results.py

--- tada/util/results.py
+++ tada/util/results.py
@@ -1,7 +1,6 @@
 """Resultstable for Tada and perf"""
 
 
-from prettytable import PrettyTable
 
 def add_resultstable(resultstable, current_size, mean, median):
     resultstable.add_row([current_size, mean, median])
