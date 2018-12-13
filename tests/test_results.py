11fe30faf6a9f033436b2f4b171aeeb0f39166f0
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-12-13 15:31:21 -0500

    Update test_results.py

--- tests/test_results.py
+++ tests/test_results.py
@@ -4,10 +4,12 @@ from prettytable import PrettyTable
 from tada.util import results
 
 resultstable = PrettyTable(['Size', 'Mean', 'Median', 'Ratio'])
+data = resultstable.get_string()
 
 
 def test_add_resultstable():
     """Add one to each column to test resultstable change"""
     resultstable1 = resultstable
     results.add_resultstable(resultstable1, 1, 1, 1, 1)
-    assert resultstable1 is not resultstable
+    data1 = resultstable1.get_string()
+    assert data1 is not data
