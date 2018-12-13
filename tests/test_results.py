c0660da21680bc67392e4d9d691269b666f053ee
Lancaster Wu <31478979+Lancasterwu@users.noreply.github.com>
2018-12-13 15:44:12 -0500

    Update test_results.py

--- tests/test_results.py
+++ tests/test_results.py
@@ -13,3 +13,20 @@ def test_add_resultstable():
     results.add_resultstable(resultstable1, 1, 1, 1, 1)
     data1 = resultstable1.get_string()
     assert data1 is not data
+
+
+def test_add_resultstable_value():
+    """Test the values inside of resultstable are what we want"""
+    resultstable1 = resultstable
+    results.add_resultstable(resultstable1, 1, 1, 1, 1)
+    for row in resultstable1:
+        row.border = False
+        row.header = False
+        data2 = row.get_string(fields=["Size"]).strip()
+        data3 = row.get_string(fields=["Mean"]).strip()
+        data4 = row.get_string(fields=["Median"]).strip()
+        data5 = row.get_string(fields=["Ratio"]).strip()
+    assert data2 is 1
+    assert data3 is 1
+    assert data4 is 1
+    assert data5 is 1
