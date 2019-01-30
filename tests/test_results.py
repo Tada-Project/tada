f4e7d4d0b76e0d509364e7a4d4db4b9ff920de21
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2019-01-30 15:50:32 -0500

    Edited test_display_resultstable and style guide

--- tests/test_results.py
+++ tests/test_results.py
@@ -31,10 +31,9 @@ def test_add_resultstable_value():
     assert data4 == "1"
     assert data5 == "1"
 
+    
 def test_display_resultstable():
     """Test to see if display_resulstable works"""
     resultstable1 = resultstable
-    resultstable2 = resultstable
     results.add_resultstable(resultstable1, 1, 1, 1, 1)
-    results.add_resultstable(resultstable2, 1, 1, 1, 1)
-    results.display_resultstable(resultstable1) == resultstable2
+    results.display_resultstable(resultstable1) != " "
