a386a4540ccfc9f9e7b43096388d255dd4db08a5
Lancasterwu <wuj@allegheny.edu>
2019-04-23 18:24:26 -0400

    added test functions

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -32,14 +32,24 @@ def test_generate_ints_makes_size_default():
 
 
 # pylint: disable=invalid-name
-def test_generate_char_makes_letter_default():
-    """Checks that requesting a generated char returns one"""
+def test_generate_int_list__makes_size_default():
+    """Checks that requesting generated ints returns them"""
     # request a single tuple with an int in it
-    requested_types = ["char"]
-    # assume the doubling experiment is at 100; not needed for this test
+    requested_types = ["int_list"]
+    # assume the doubling experiment is at 100
     current_size = 100
-    # the default generator will return a tuple with the default character in it
-    expected_tuple = (generate.DEFAULT_VALUE_CHAR,)
     # generate the data for the requested_types and the current_size
     generated_data = generate.generate_data(requested_types, current_size)
-    assert generated_data == expected_tuple
+    assert generated_data != None
+
+
+# pylint: disable=invalid-name
+def test_generate_char_list_makes_letter_default():
+    """Checks that requesting a generated char returns one"""
+    # request a single tuple with an int in it
+    requested_types = ["char_list"]
+    # assume the doubling experiment is at 100; not needed for this test
+    current_size = 100
+    # generate the data for the requested_types and the current_size
+    generated_data = generate.generate_data(requested_types, current_size)
+    assert generated_data != None
