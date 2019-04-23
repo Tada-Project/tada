376c38904e28e4e894e0f393b764d5c9793e9de1
Spencer Huang <31478964+huangs1@users.noreply.github.com>
2019-04-23 18:48:31 -0400

    Remove trailing space

--- tada/util/results.py
+++ tada/util/results.py
@@ -4,7 +4,7 @@ def add_resultstable(resultstable, current_size, mean, median, ratio):
     """Add elements into the resultstable."""
     resultstable.add_row([current_size, mean, median, ratio])
 
-    
+
 def display_resultstable(resultstable):
     """Print out the resultstable."""
     print(resultstable)
