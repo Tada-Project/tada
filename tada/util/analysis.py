62da8770fd3c2dca127db1ea6c22358099a97397
Lancaster Wu <wuj@allegheny.edu>
2019-04-28 23:05:27 -0400

    added docstring and fixed C0325

--- tada/util/analysis.py
+++ tada/util/analysis.py
@@ -2,13 +2,15 @@
 
 
 def analyze_big_oh(ratio):
+    """analyze big oh"""
     if (0 <= ratio < 1.5):
-        print("constant or logarithmic")
+        output = "constant or logarithmic"
     elif (1.5 <= ratio < 3):
-        print("linear or linearithmic")
+        output = "linear or linearithmic"
     elif (3 <= ratio < 5):
-        print("quadratic")
+        output = "quadratic"
     elif (5 <= ratio < 10):
-        print("cubic")
+        output = "cubic"
     else:
-        print("exponential")
+        output = "exponential"
+    print(output)
