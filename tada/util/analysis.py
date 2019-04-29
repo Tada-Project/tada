8a97300dda4ce761a3d73f298311df47c1f4a1f0
Lancaster Wu <wuj@allegheny.edu>
2019-04-28 23:12:13 -0400

    fixed C0325 linting error

--- tada/util/analysis.py
+++ tada/util/analysis.py
@@ -3,14 +3,14 @@
 
 def analyze_big_oh(ratio):
     """analyze big oh"""
-    if (0 <= ratio < 1.5):
-        output = "constant or logarithmic"
-    elif (1.5 <= ratio < 3):
-        output = "linear or linearithmic"
-    elif (3 <= ratio < 5):
-        output = "quadratic"
-    elif (5 <= ratio < 10):
-        output = "cubic"
+    if 0 <= ratio < 1.5:
+        output = "O(1) constant or O(logn) logarithmic"
+    elif 1.5 <= ratio < 3:
+        output = "O(n) linear or O(nlogn) linearithmic"
+    elif 3 <= ratio < 5:
+        output = "O(n^2) quadratic"
+    elif 5 <= ratio < 10:
+        output = "O(n^3) cubic"
     else:
-        output = "exponential"
+        output = "O(c^n) exponential"
     print(output)
