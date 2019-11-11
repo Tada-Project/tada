2048436878b6aa8d098f9e6a90378d0a4ff80f2b
enpuyou <youe2@allegheny.edu>
2019-11-10 20:26:17 -0500

    Add argument for path to json

--- tada/util/arguments.py
+++ tada/util/arguments.py
@@ -63,6 +63,12 @@ def parse(args):
         default=constants.RUNNINGTIME,
         help="The maximum running time",
     )
+    parser.add_argument(
+        "--strategy",
+        required=False,
+        type=str,
+        help="The path to the jsonschema",
+    )
     # parse the arguments and return the finished result
     arguments_finished = parser.parse_args(args)
     return arguments_finished
