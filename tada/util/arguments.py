dcb47d4fa969be79bf9739cde4020c7cdaba8ce2
enpuyou <youe2@allegheny.edu>
2019-12-31 01:14:33 -0500

    Minor reformat

--- tada/util/arguments.py
+++ tada/util/arguments.py
@@ -43,29 +43,33 @@ def parse(args):
         help="Parameter types for function to analyze",
     )
     parser.add_argument(
+        "--schema",
+        required=False,
+        type=str,
+        help="The path to the jsonschema",
+    )
+    parser.add_argument(
         "--startsize",
         required=False,
         type=int,
         default=constants.SIZE_START,
-        help="The starting size of doubling experiment",
+        help="Starting size of the doubling experiment",
     )
     parser.add_argument(
         "--steps",
         required=False,
         type=int,
         default=constants.STEPS,
-        help="The maximum rounds of experiment",
+        help="Maximum rounds of experiment",
     )
     parser.add_argument(
         "--runningtime",
         required=False,
         type=int,
         default=constants.RUNNINGTIME,
-        help="The maximum running time",
+        help="Maximum running time",
     )
-    parser.add_argument(
-        "--schema", required=False, type=str, help="The path to the jsonschema"
-    )
+
     # parse the arguments and return the finished result
     arguments_finished = parser.parse_args(args)
     return arguments_finished
