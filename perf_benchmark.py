de7118a8ac851fcb815d9e92f42c8510e9395a80
enpuyou <youe2@allegheny.edu>
2019-12-30 19:19:20 -0500

    update benchmark with three approaches

--- perf_benchmark.py
+++ perf_benchmark.py
@@ -27,33 +27,41 @@ if __name__ == "__main__":
     analyzed_function = getattr(
         analyzed_module, configuration.get_function(tada_configuration_dict)
     )
-
     # read the chosen_size
     chosen_size = read.read_experiment_size()
     # configure perf
     runner = pyperf.Runner()
-
-    fakefunction = generate.generate_strategy(
-        generate.generate_fake_hypothesis,
-        configuration.get_schema_path(tada_configuration_dict),
-        chosen_size,
-    )
-
-    fakefunction()
-    f = open("data.txt", "r")
-    raw_data = f.read()
-    formatted_data = raw_data[1:-1]
-    data = list(formatted_data.split(","))
-
     # give a by-configuration name to the experiment
     current_experiment_name = configuration.get_experiment_name(
         tada_configuration_dict, chosen_size
     )
     # set the name of the experiment for perf
     runner.metadata[constants.DESCRIPTION_METANAME] = current_experiment_name
-    # run the benchmark using the bench_func from perf
-    current_benchmark = runner.bench_func(
-        current_experiment_name, run.run_benchmark, analyzed_function, data
-    )
+    # Get type
+    func_type = configuration.get_types(tada_configuration_dict)
+    if func_type[0] == "hypothesis_save":
+        func_type = configuration.get_schema_path(tada_configuration_dict)
+    if func_type[0] == "hypothesis":
+        analyzed_function = generate.generate_strategy(
+            analyzed_function,
+            configuration.get_schema_path(tada_configuration_dict),
+            chosen_size,
+        )
+        func_type = configuration.get_schema_path(tada_configuration_dict)
+        current_benchmark = runner.bench_func(
+            current_experiment_name,
+            run.run_benchmark,
+            analyzed_function,
+        )
+    else:
+        current_benchmark = runner.bench_func(
+            current_experiment_name,
+            run.run_benchmark,
+            analyzed_function,
+            *generate.generate_data(
+                func_type,
+                chosen_size,
+            ),
+        )
     # save the perf results from running the benchmark
     save.save_benchmark_results(current_benchmark, current_experiment_name)
