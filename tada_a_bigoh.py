f06cf89344b1d2414a6acf43d18f69cd38a6839d
Lancasterwu <wuj@allegheny.edu>
2019-12-31 21:15:17 -0500

    store average benchmark loops and loops growth ratio to the csv file

--- tada_a_bigoh.py
+++ tada_a_bigoh.py
@@ -20,7 +20,6 @@ from tada.util import results
 
 if __name__ == "__main__":
     start_time = time.time()
-
     # display the welcome message
     display.display_welcome_message()
     # read and verify the command-line arguments
@@ -33,6 +32,8 @@ if __name__ == "__main__":
     last_last_size = 0
     count = 0
     current_size = tada_arguments.startsize
+    total_loop_list = []
+    sum_of_loops = 0
     # incorrect arguments, exit program
     if did_verify_arguments is False:
         print("Incorrect command-line arguments.")
@@ -72,6 +73,11 @@ if __name__ == "__main__":
                 + configuration.get_experiment_name(vars(tada_arguments), current_size)
                 + constants.JSON_EXT
             )
+            # Numbers of benchmark run which include one warmup and twenty excution
+            # print(current_benchmark.get_nrun())
+            # Total Runtime of the benchmark excution
+            # print(current_benchmark.get_total_duration())
+            total_loop_list.append(current_benchmark.get_total_loops())
             # perform additional analysis of the results
             # reminder: print('Values {0}'.format(current_benchmark.get_values()))
             mean = current_benchmark.mean()
@@ -144,6 +150,11 @@ if __name__ == "__main__":
         last_exp_run_metadata = readlastjson["benchmarks"][0]["runs"][0]["metadata"]
         constants.CPU_TEMP = last_exp_run_metadata["cpu_temp"]
         constants.MEM_MAX_RSS = last_exp_run_metadata["mem_max_rss"]
+        for item in total_loop_list:
+            sum_of_loops += item
+        constants.PYPERF_AVG_EXPERIMENT_ROUNDS = sum_of_loops / len(total_loop_list)
+        if len(total_loop_list) >= 2:
+            constants.PYPERF_LAST_TWO_EXPERIMENT_ROUNDS = total_loop_list[-1] / total_loop_list[-2]
         df = pd.read_csv("experiment_data.csv")
         # EXPERIMENT_RELIABILITY, CPU_TYPE, CPU_TEMP, TOTAL_RUNNING_TIME, QUIT_BY_MAX_RUNTIME, QUIT_BY_INDICATOR, QUIT_BY_BACKFILL, MEM_MAX_RSS, OS, INDICATOR_VALUE, BACKFILL_TIMES, PYPERF_AVG_EXPERIMENT_ROUNDS, NAME_OF_EXPERIMENT
         df_new = pd.DataFrame(
@@ -160,9 +171,10 @@ if __name__ == "__main__":
                 "INDICATOR_VALUE": constants.INDICATOR_VALUE,
                 "BACKFILL_TIMES": constants.BACKFILL_TIMES,
                 "PYPERF_AVG_EXPERIMENT_ROUNDS": constants.PYPERF_AVG_EXPERIMENT_ROUNDS,
+                "PYPERF_LAST_TWO_EXPERIMENT_ROUNDS_RATIO": constants.PYPERF_LAST_TWO_EXPERIMENT_ROUNDS_RATIO,
                 "NAME_OF_EXPERIMENT": constants.NAME_OF_EXPERIMENT,
                 "PYTHON_VERSION": constants.PYTHON_VERSION,
             },
             index=[1],
         )
-        df_new.to_csv("experiment_data.csv", index=False, header=False, mode="a")
+        # df_new.to_csv("experiment_data.csv", index=False, header=False, mode="a")
