d9fa9cd9214b104fbce1f45a6964e087aa8b550c
enpuyou <youe2@allegheny.edu>
2020-06-26 07:27:33 -0400

    Add color to display

--- tada/util/display.py
+++ tada/util/display.py
@@ -24,3 +24,17 @@ def display_output(timing_output, to_print=False):
     """Display the timing output as long as it is not empty"""
     if (timing_output != "") & (to_print is True):
         print(timing_output)
+
+
+class FontColor:
+    Black = '\u001b[30m'
+    Red = '\u001b[31m'
+    Green = '\u001b[32m\u001b[1m'
+    Yellow = '\u001b[33m'
+    Blue = '\u001b[34m'
+    Magenta = '\u001b[35m'
+    Cyan = '\u001b[36m'
+    White = '\u001b[37m'
+    Reset = '\u001b[0m'
+    Bold = '\u001b[1m'
+    Underline = '\u001b[4m'
