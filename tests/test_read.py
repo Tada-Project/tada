a70e4d1d18afb9fb9ce51696ec00f2dc9bbafb79
enpuyou <youe2@allegheny.edu>
2020-06-30 16:02:28 -0400

    Update read test case

--- tests/test_read.py
+++ tests/test_read.py
@@ -3,13 +3,9 @@
 from tada.util import read
 
 
-def test_read_experiment_size():
-    """Checks that read experimnet size properly return one"""
-    size = read.read_experiment_size()
-    assert size is not None
-
-
-def test_read_directory_size():
-    """Checks that read directory properly return one"""
-    directory = read.read_directory()
-    assert directory is not None
+def test_read_value(tmpdir):
+    """Checks that read value properly return one"""
+    path = tmpdir.mkdir("sub").join("hello.txt")
+    path.write("Hello World")
+    output = read.read_value(path)
+    assert output is not None
