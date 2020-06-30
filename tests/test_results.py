77a4e9d30cf2f8028728162f0278ee337e1b583c
enpuyou <youe2@allegheny.edu>
2020-06-30 05:33:35 -0400

    Add test cases for comparison functions

--- tests/test_results.py
+++ tests/test_results.py
@@ -43,7 +43,9 @@ def test_to_markdown_table():
     """Test to see if to_markdown_table functions properly."""
     results_table = PrettyTable(["Size", "Mean", "Median", "Ratio"])
     output = results.to_markdown_table(results_table)
-    expect = """ Size | Mean | Median | Ratio \n------|------|--------|-------"""
+    expect = (
+        """ Size | Mean | Median | Ratio \n------|------|--------|-------"""
+    )
     assert expect == output
 
 
@@ -57,3 +59,29 @@ def test_display_resultstable_to_markdown(capsys):
 ------|------|--------|-------\n\
   1   |  1   |   1    |   1   \n"""
     assert captured.out == expect
+
+
+def test_greatest_common_size():
+    """Test if can find the greatest common size of two experiments"""
+    input_data = {
+        "insertion_sort": {50: [7.928830701700847e-06, 6.951078582763674e-06],},
+        "bubble_sort": {
+            50: [0.00012845424799804686, 0.00011387420361328127],
+            100: [0.0004576137537760417, 0.00040870740234375],
+        },
+    }
+    size, _ = results.greatest_common_size(input_data)
+    expected = 50
+    assert size == expected
+
+
+def test_compare(capsys):
+    """Test if comparison functions correctly"""
+    size = 100
+    experiment_data = {
+        "insertion_sort": [1.436614706624349e-05, 1.3248109497070312e-05],
+        "bubble_sort": [0.0004576137537760417, 0.00040870740234375],
+    }
+    results.compare(size, experiment_data)
+    standard_out = capsys.readouterr()
+    assert standard_out is not None
