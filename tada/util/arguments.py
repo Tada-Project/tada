eeb60768e53c7a902602dbcbfb71887b0a435838
Lancaster Wu <wuj@allegheny.edu>
2020-07-01 14:11:14 -0400

    fixed some bad indentations

--- tada/util/arguments.py
+++ tada/util/arguments.py
@@ -120,20 +120,20 @@ def parse(args):
         help="Show results table in markdown format",
     )
     parser.add_argument(
-    "--level",
-    required=False,
-    type=int,
-    default=constants.LEVEL,
-    help="The level of nested data structure to apply doubling experiment",
+        "--level",
+        required=False,
+        type=int,
+        default=constants.LEVEL,
+        help="The level of nested data structure to apply doubling experiment",
     )
     parser.add_argument(
-    "--position",
-    required=False,
-    nargs="+",
-    type=int,
-    default=constants.POSITION,
-    help="""The position of input data to double in the multivariable
-    doubling experiment. Must be the last argument""",
+        "--position",
+        required=False,
+        nargs="+",
+        type=int,
+        default=constants.POSITION,
+        help="""The position of input data to double in the multivariable
+        doubling experiment. Must be the last argument""",
     )
     # parse the arguments and return the finished result
     arguments_finished = parser.parse_args(args)
