2282b4fb67ef8d94d8febd5af80a8a1dd6dee07b
Lancaster Wu <wuj@allegheny.edu>
2020-07-08 15:03:19 -0400

    fixed a bug where maxsize only works for hypothesis

--- tada_a_bigoh.py
+++ tada_a_bigoh.py
@@ -69,14 +69,13 @@ def tada(tada_arguments):
                     constants.QUIT_BY_BACKFILL = 1
                     dis.display_output("\nQuit due to backfill twice", to_print)
                     break
-            if "hypothesis" in tada_arguments.types[0]:
-                if current_size >= tada_arguments.maxsize:
-                    constants.QUIT_BY_MAX_SIZE = 1
-                    dis.display_output(
-                        "\nQuit due to reaching max size: " + str(tada_arguments.maxsize),
-                        to_print,
-                    )
-                    break
+            if current_size >= tada_arguments.maxsize:
+                constants.QUIT_BY_MAX_SIZE = 1
+                dis.display_output(
+                    "\nQuit due to reaching max size: " + str(tada_arguments.maxsize),
+                    to_print,
+                )
+                break
             dis.display_start_message(current_size, tada_arguments.function)
             current_output, current_error = run.run_command(
                 constants.PYTHON_EXEC
