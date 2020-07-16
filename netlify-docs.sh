628dd3c47ecc83a27f16439206d0efe04cc598c6
enpuyou <youe2@allegheny.edu>
2020-07-16 03:10:38 -0400

    modify netlify-docs command

--- netlify-docs.sh
+++ netlify-docs.sh
@@ -1,36 +1,6 @@
 #!/usr/bin/env bash
 
-# set -x
-
-# cd /tmp
-# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-
-# python get-pip.py --user
-
-# cd -
-
-# python -m pip install --user mkdocs
-# python -m mkdocs build
-# brew install pyenv
-# pyenv install 3.7
-# pipenv install --dev
-# pipenv --python 3.7
-# export PIPENV_IGNORE_VIRTUALENVS=1
 
 
-# pip install pipenv
-# pipenv install mkdocs
-# pipenv run mkdocs build
-
-
-set -x
-set -e
-# Install pip
-cd /tmp
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.6 get-pip.py --user
-cd -
-
-python3.6 -m pip install --user mkdocs
-
-python3.6 -m mkdocs build
+pipenv shell
+mkdocs build
