12007b194ac00b65a35a4217cb48ce57df3ed36d
enpuyou <youe2@allegheny.edu>
2020-07-16 03:52:25 -0400

    Remove command failing build

--- netlify-docs.sh
+++ netlify-docs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# force pipenv to create its own new virtualenv
-export PIPENV_IGNORE_VIRTUALENVS=1;
 # install pipenv
 pip install pipenv
 # install all dev dependencies (including mkdocs)
