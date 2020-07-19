8d6ed3a0cbda770120dbea48fdd8ca2c408caae2
enpuyou <youe2@allegheny.edu>
2020-07-19 04:09:21 -0400

    Add spaces to better align the text

--- tada/util/display.py
+++ tada/util/display.py
@@ -8,7 +8,7 @@ init(autoreset=True)
 def welcome_message() -> None:
     """Display a welcome message"""
     return "\n\t\tTada!: auTomAtic orDer-of-growth Analysis!\n\t\t  \
-https://github.com/Tada-Project/tada/\n\tFor Help Information Type: \
+https://github.com/Tada-Project/tada/\n\t  For Help Information Type: \
 python tada_a_bigoh.py -h"
 
 
