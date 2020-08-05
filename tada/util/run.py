43b4f878a4f4c01cb3283aad84e352bf02608cc0
enpuyou <youe2@allegheny.edu>
2020-08-04 23:06:47 -0400

    Change work dir to where the package installed

--- tada/util/run.py
+++ tada/util/run.py
@@ -3,11 +3,15 @@
 import subprocess
 from typing import Tuple
 
+from pathlib import Path
+
+path = Path(__file__).parent.parent.absolute()
+
 
 def run_command(command: str) -> Tuple[bytes, bytes]:
     """Run a command and return the output and error code"""
     process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=path,
     )
     output, error = process.communicate()
     return output, error
